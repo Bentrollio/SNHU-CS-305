c4b4c0d9658cce3e69ef09853a65931467ec201b
Alex Baires <109702084+Bentrollio@users.noreply.github.com>
2024-06-05 15:56:09 -0400

    Working bytesToHex function with SHA-256

--- CS 305 Module Five Checksum Verification Assignment Code/module5_skel_student/src/main/java/com/snhu/sslserver/ServerApplication.java
+++ CS 305 Module Five Checksum Verification Assignment Code/module5_skel_student/src/main/java/com/snhu/sslserver/ServerApplication.java
@@ -1,3 +1,12 @@
+/*
+ * Alex Baires
+ * CS-305 Software Security
+ * Module 5-1 Assignment: Checksum Verification
+ * 
+ * 6/9/2024
+ * 
+ */
+
 package com.snhu.sslserver;
 
 import org.springframework.boot.SpringApplication;
@@ -5,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 @SpringBootApplication
@@ -18,11 +29,53 @@ public class ServerApplication {
 
 @RestController
 class ServerController{
-//FIXME:  Add hash function to return the checksum value for the data string that should contain your name.    
+	
+	/**
+	 * bytesToHex
+	 * 
+	 * Adapted from:
+	 * https://www.tutorialspoint.com/java_cryptography/java_cryptography_message_digest.htm#:~:text=You%20can%20generate%20the%20message,digest%20using%20the%20digest%20method.
+	 * 
+	 * @param userData - A string to be converted into a SHA-256 checksum.
+	 * @return hex - A string containing the HexString converted message as output.
+	 */
+	
+	public String bytesToHex(String userData) {
+		
+		String hex = ""; // Initialize the local variable
+		
+		// Create MessageDigest object
+    	try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			
+			// Pass data to the created MessageDigest object
+			md.update(userData.getBytes());
+			
+			// Computes the message digest
+			byte[] digest = md.digest();
+			System.out.println(digest);
+			
+			// Converts the byte array into HexString object
+			StringBuffer hexString = new StringBuffer();
+			
+			for (int i = 0; i < digest.length; ++i) {
+				hexString.append(Integer.toHexString(0xFF & digest[i]));
+			}
+			
+			hex = hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		return hex;
+	}
+	
     @RequestMapping("/hash")
     public String myHash(){
-    	String data = "Hello Joe Smith!";
-       
-        return "<p>data:"+data;
+    	
+    	// Update 6-3 - Added student name.
+    	String data = "Hello Alex Baires!";
+    	
+        return "<p>data: " + data + "<p>SHA-256: CheckSum Value: " + bytesToHex(data);
     }
 }
